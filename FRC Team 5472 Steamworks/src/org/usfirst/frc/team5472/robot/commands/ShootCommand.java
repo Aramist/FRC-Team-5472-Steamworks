a4091f50fba7a733b53a6ffa8f7ba564d998d88d
Robotics-05 <Robotics-05@Robotics-05-HP.ahschool.com>
2017-02-08 17:56:39 -0500

    Worked on ShootCommand

--- FRC Team 5472 Steamworks/src/org/usfirst/frc/team5472/robot/commands/ShootCommand.java
+++ FRC Team 5472 Steamworks/src/org/usfirst/frc/team5472/robot/commands/ShootCommand.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team5472.robot.commands;
 
 import org.usfirst.frc.team5472.robot.Robot;
+import org.usfirst.frc.team5472.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
@@ -22,14 +23,18 @@ public class ShootCommand extends Command {
 	public void execute(){
 		if (Robot.oi.stick1.getRawButton(5))//alternate idea - if boiler is recognized by camera & contingency plan is to have them be able to manually turn it
 		{
+			double x = (reference.getRawButton(RobotMap.susanButton)) ? 0.3 : 0.0;//not sure how slowly it needs to be spin to allow the camera to register the boiler
 			//repeat this task until it is correctly oriented towards boiler
-			 Robot.shootSubsystem.setSusanMotor(0.8);
+			 Robot.shootSubsystem.setSusanMotor(x);
 		}
 		if (Robot.oi.stick1.getRawButton(6))
-		Robot.shootSubsystem.setShoot1(0.8);// will most likely have some constant value
+		{
+			double x = (reference.getRawButton(RobotMap.shootButton)) ? 0.4 : 0.0;	
+		Robot.shootSubsystem.setShoot1(x);// will most likely have some constant value
 		//double shoot; -> need to come up with a way to adjust speed based on driver input sensor
-		Robot.shootSubsystem.setShoot2(0.8);//arbitrary value ~ need to change depending on angle to boiler
-		
+		Robot.shootSubsystem.setShoot2(2*x);//arbitrary value ~ need to change depending on angle to boiler
+		//the smaller wheel will (hopefully) be in ratio to larger wheel, which is shoot1
+		}
 		
 	}
 	
